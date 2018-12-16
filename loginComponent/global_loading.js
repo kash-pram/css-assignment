346fe217d8e680116c2d1383e20b21efd83b3878
admin <admin@onlimages.com>
2018-12-16 23:27:29 +0530

    basics WIP

---
+++ loginComponent/global_loading.js
@@ -0,0 +1,7 @@
+// var gb_loadingHTML_OLD = "<div class='spinner'>\
+//                         <div class='bounce1'></div>\
+//                         <div class='bounce2'></div>\
+//                         <div class='bounce3'></div>\
+//                     </div>";
+
+var gb_loadingHTML = "<div class='loadingspinner'></div>";
